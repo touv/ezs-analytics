65593e67473697549db4cd70451132119f2335d7
François Parmentier <francois.parmentier@gmail.com>
2018-08-08 16:06:13 +0200

    Remove analytics statements (except one)

--- src/globals.js
+++
@@ -1,3 +0,0 @@
-import Store from './store';
-
-export const store = new Store('global');
--- src/index.js
+++ src/index.js
@@ -1,51 +1,5 @@
 import count from './count';
-import distinct from './distinct';
-import graph from './graph';
-import pair from './pair';
-import pluck from './pluck';
-import keys from './keys';
-import maximizing from './maximizing';
-import merging from './merging';
-import minimizing from './minimizing';
-import reducing from './reducing';
-import exploding from './exploding';
-import summing from './summing';
-import groupingByEquality from './groupingByEquality';
-import groupingByLevenshtein from './groupingByLevenshtein';
-import groupingByHamming from './groupingByHamming';
-import tune from './tune';
-import value from './value';
-import sort from './sort';
-import topics from './topics';
-import segment from './segment';
-import stuck from './stuck';
-import unstuck from './unstuck';
-import output from './output';
-import slice from './slice';
 
 export default {
     count,
-    distinct,
-    graph,
-    pair,
-    pluck,
-    keys,
-    minimizing,
-    maximizing,
-    merging,
-    reducing,
-    exploding,
-    summing,
-    groupingByEquality,
-    groupingByLevenshtein,
-    groupingByHamming,
-    tune,
-    value,
-    sort,
-    topics,
-    segment,
-    stuck,
-    unstuck,
-    output,
-    slice,
 };
