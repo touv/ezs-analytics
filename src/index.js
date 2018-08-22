f2e67b634b2c563269017b71cf2e597606244997
François Parmentier <francois.parmentier@gmail.com>
2018-08-22 11:10:59 +0200

    Remove count from this module
    It was the starting example.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import count from './count';
 import TEEFTTokenize from './tokenize';
 
 export default {
-    count,
     TEEFTTokenize,
     // TEEFTTag,
     // TEEFTSanitize,
--- test/tests.js
+++ test/tests.js
@@ -4,31 +4,6 @@ const ezs = require('ezs');
 
 ezs.use(require('../lib'));
 
-describe('test', () => {
-    it('count', (done) => {
-        const res = [];
-        from([
-            { a: 'x' },
-            { a: 't' },
-            { a: 't' },
-            { a: 'x' },
-        ])
-            .pipe(ezs('count', { path: 'a' }))
-            .on('data', (chunk) => {
-                assert(typeof chunk === 'object');
-                res.push(chunk);
-            })
-            .on('end', () => {
-                assert.equal(4, res.length);
-                assert.equal(1, res[0].value);
-                assert.equal(1, res[1].value);
-                assert.equal(1, res[2].value);
-                assert.equal(1, res[3].value);
-                done();
-            });
-    });
-});
-
 describe('tokenize', () => {
     it('should split ascii simple words', (done) => {
         let res = [];
