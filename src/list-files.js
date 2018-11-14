069ab9183951b9fb4580d260de0bf1a43aaea12c
François Parmentier <francois.parmentier@gmail.com>
2018-11-14 10:35:35 +0100

    :sparkles: Add ListFiles skeleton

---
+++ src/list-files.js
@@ -0,0 +1,18 @@
+/**
+ * Take an array of directory paths as input, a pattern, and returns a list of
+ * file paths matching the pattern in the directories from the input.
+ *
+ * @param {String}  [pattern="*"]   pattern for files (ex: "*.txt")
+ * @returns {<Array<String>>}   an array of file paths
+ */
+function ListFiles(data, feed) {
+    if (this.isLast()) {
+        return feed.close();
+    }
+    feed.write();
+    feed.end();
+}
+
+export default {
+    ListFiles,
+};
